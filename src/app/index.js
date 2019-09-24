ab254a50747063b902b316c041c2787ec89ffd20
Zhenghao Jia <cherie-xia@hotmail.com>
2019-09-24 22:03:22 +0800

    dashboard

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,6 @@
 import { store } from "./store";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "./components/Main";
 
-console.log(store.getState());
+ReactDOM.render(<Main />, document.getElementById("app"));
