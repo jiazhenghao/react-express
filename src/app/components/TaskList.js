9e1d2e0566474a2e03a2ee6521a03aaa305422d1
Zhenghao Jia <cherie-xia@hotmail.com>
2019-10-08 23:06:51 +0800

    saga

--- src/app/components/TaskList.js
+++ src/app/components/TaskList.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import { requestTaskCreation } from "../store/mutations";
 
-export const TaskList = ({ tasks, name }) => (
+export const TaskList = ({ tasks, name, id, createNewTask }) => (
   <>
     <h3>{name}</h3>
     <div>
       {tasks.map(task => (
-        <div key={task.name}>{task.name}</div>
+        <div key={task.id}>{task.name}</div>
       ))}
     </div>
+    <button onClick={() => createNewTask(id)}>create</button>
   </>
 );
 
@@ -21,4 +23,16 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export const ConnectedTaskList = connect(mapStateToProps)(TaskList);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    createNewTask(id) {
+      console.log("Creating new task ...", id);
+      dispatch(requestTaskCreation(id));
+    }
+  };
+};
+
+export const ConnectedTaskList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TaskList);
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,6 +1,43 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { defaultState } from "../../server/defaultState";
+import { logger } from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import * as sagas from "./sagas.mock";
+import * as mutations from "./mutations";
 
-export const store = createStore((state = defaultState, action) => {
-  return state;
-});
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = createStore(
+  combineReducers({
+    tasks(tasks = defaultState.tasks, action) {
+      switch (action.type) {
+        case mutations.CREATE_TASK:
+          return [
+            ...tasks,
+            {
+              id: action.taskID,
+              name: "New Task",
+              group: action.groupID,
+              owner: action.ownerID,
+              isComplete: false
+            }
+          ];
+        default:
+          break;
+      }
+      return tasks;
+    },
+    comments(comments = defaultState.comments) {
+      return comments;
+    },
+    groups(groups = defaultState.groups) {
+      return groups;
+    },
+    users(users = defaultState.users) {
+      return users;
+    }
+  }),
+  applyMiddleware(logger, sagaMiddleware)
+);
+
+sagaMiddleware.run(sagas.taskCreationSaga);
