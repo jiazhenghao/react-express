c6c8e293100bfd2f93602318012764a2e499e4a3
Zhenghao Jia <cherie-xia@hotmail.com>
2019-10-12 23:12:53 +0800

    redux-saga

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -6,18 +6,30 @@ import { Router, Route } from "react-router-dom";
 import { history } from "../store/history";
 import { ConnectedNavigation } from "./Navigation";
 import { ConnectTaskDetail } from "./TaskDetail";
+import { Redirect } from "react-router";
+import { ConnectedLogin } from "../components/Login";
+
+const RouteGuard = Component => ({ match }) => {
+  console.info("Route guard", match);
+  if (!store.getState().session.authenticated) {
+    return <Redirect to="/" />;
+  } else {
+    return <Component match={match} />;
+  }
+};
 
 export const Main = () => (
   <Router history={history}>
     <Provider store={store}>
       <div>
         <ConnectedNavigation />
-        <Route exact path="/dashboard">
-          <ConnectedDashboard />
+        <Route exact path="/" component={ConnectedLogin}></Route>
+        <Route exact path="/dashboard" render={RouteGuard(ConnectedDashboard)}>
+          {/* <ConnectedDashboard /> */}
         </Route>
-        <Route exact path="/task/:id">
+        <Route exact path="/task/:id" render={RouteGuard(ConnectTaskDetail)}>
           {/* <ConnectTaskDetail match={match} /> */}
-          <ConnectTaskDetail />
+          {/* <ConnectTaskDetail /> */}
         </Route>
       </div>
     </Provider>
--- src/server/defaultState.js
+++ src/server/defaultState.js
@@ -1,16 +1,21 @@
+import md5 from "md5";
+
 export const defaultState = {
+  // session: {
+  //   authenticated: false
+  // },
   users: [
     {
       id: "U1",
       name: "Dev",
-      //passwordHash: md5("TUPLES"),
-      friends: [`U2`]
+      passwordHash: md5("TUPLES")
+      //friends: [`U2`]
     },
     {
       id: "U2",
       name: "C. Eeyo",
-      //passwordHash: md5("PROFITING"),
-      friends: []
+      passwordHash: md5("PROFITING")
+      //friends: []
     }
   ],
   groups: [
